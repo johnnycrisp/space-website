449881842da64f55fe430834e27c52ff463c9f71
Johnny Crisp <johnnycrisp2@gmail.com>
2022-11-09 18:55:02 +0100

    add state to description section

--- src/App.js
+++ src/App.js
@@ -5,13 +5,53 @@ import Destination from './components/Destination/Destination'
 import Nav from './components/Nav/Nav';
 import Crew from './components/Crew/Crew';
 import Technology from './components/Technology/Technology';
+import IMGMOON from './assets/destination/image-moon.png'
+import IMGMARS from './assets/destination/image-mars.png'
+import IMGEUROPA from './assets/destination/image-europa.png'
+import IMGTITAN from './assets/destination/image-titan.png'
+
+const PLANETS = [
+    {
+      name: 'MOON',
+      id: 1,
+      img: IMGMOON,
+      text: 'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.',
+      distance: '384,400 KM',
+      travelTime: '3 DAYS'
+    },
+    {
+      name: 'MARS',
+      id: 2,
+      img: IMGMARS,
+      text: 'Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!',
+      distance: '225 MIL. KM',
+      travelTime: '9 MONTHS'
+    },
+    {
+      name: 'EUROPA',
+      id: 3,
+      img: IMGEUROPA,
+      text: 'The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.',
+      distance: '628 MIL. KM',
+      travelTime: '3 YEARS'
+    },
+    {
+      name: 'TITAN',
+      id: 4,
+      img: IMGTITAN,
+      text: 'The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.',
+      distance: '1.6 BIL. KM',
+      travelTime: '7 YEARS'
+    }
+  ];
 
 function App() {
+
   return (
     <div className="App">
       <Nav />
       <Header />
-      <Destination />
+      <Destination planets={PLANETS} />
       <Crew />
       <Technology />
     </div>
--- src/components/Destination/Destination.jsx
+++ src/components/Destination/Destination.jsx
@@ -1,39 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Container from 'react-bootstrap/Container'
 import './destination.scss'
-import IMG from '../../assets/destination/image-moon.png'
 import Nav from 'react-bootstrap/Nav'
+import Button from 'react-bootstrap/Button'
 // import IMG from ''
 
-const Destination = () => {
+const Destination = ({planets}) => {
+
+  const [planet, setPlanet] = useState(0);
+
+
   return (
     <div className="destination__wrapper">
         
             <h5 className="destination__header"><span>01</span>PICK YOUR DESTINATION</h5>
             <Container>
             <div className="image__container">
-                <img src={IMG} alt="IMG Moon" />
+                <img src={planets[planet].img} alt={planet} />
             </div>
              <div className="planet__info-all"><div className="planet__info">
                 <Nav className="nav__links">
-                  <Nav.Link className="nav__link" href="#action1">MOON</Nav.Link>
-                  <Nav.Link className="nav__link" href="#action2">MARS</Nav.Link>
-                  <Nav.Link className="nav__link" href="#action3">EUROPA</Nav.Link>
-                  <Nav.Link className="nav__link" href="#action4">TITAN</Nav.Link>
+                  <Button variant="btn-outline" className="nav__link nav-link" onClick={() => setPlanet(0)} >MOON</Button>
+                  <Button variant="btn-outline" className="nav__link nav-link" onClick={() => setPlanet(1)} >MARS</Button>
+                  <Button variant="btn-outline" className="nav__link nav-link" onClick={() => setPlanet(2)} >EUROPA</Button>
+                  <Button variant="btn-outline" className="nav__link nav-link" onClick={() => setPlanet(3)} >TITAN</Button>
                 </Nav>
-            <h2 className="planet__header">MOON</h2>
-            <p className="planet__text">See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.</p>
+            <h2 className="planet__header">{planets[planet].name}</h2>
+            <p className="planet__text">{planets[planet].text}</p>
             </div> 
             <hr className="my-4"/>
             <div className="dest__info"> 
            
                 <div className="distance__info">
                     <p className="sub-head-1">AVG. DISTANCE</p>
-                    <p className="sub-head-2">384,400 KM</p>
+                    <p className="sub-head-2">{planets[planet].distance}</p>
                 </div>
                 <div className="travel__info">
                     <p className="sub-head-1">EST. TRAVEL TIME</p>
-                    <p className="sub-head-2">3 DAYS</p>
+                    <p className="sub-head-2">{planets[planet].travelTime}</p>
                 </div></div>
                 
             </div>
