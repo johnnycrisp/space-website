5b5e22e35e8b8d6cad3ef7ba12586f51cdfa30f2
Johnny Crisp <johnnycrisp2@gmail.com>
2022-11-11 14:29:16 +0100

    fix styling responsive

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ const PLANETS = [
     {
       role: 'MISSION SPECIALIST',
       id: 1,
-      name: 'MARK SHUTTLEWORTH',
+      name: 'MARK STEVENS',
       img: IMGMARK,
       text: 'Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.'
     },
--- src/components/Destination/Destination.jsx
+++ src/components/Destination/Destination.jsx
@@ -17,27 +17,28 @@ const Destination = ({planets}) => {
             <div className="image__container">
                 <img src={planets[planet].img} alt={planet} />
             </div>
-             <div className="planet__info-all"><div className="planet__info">
-                <Nav className="nav__links">
-                  <Button variant="btn-outline" className="nav__link nav-link" onClick={() => setPlanet(0)} >MOON</Button>
-                  <Button variant="btn-outline" className="nav__link nav-link" onClick={() => setPlanet(1)} >MARS</Button>
-                  <Button variant="btn-outline" className="nav__link nav-link" onClick={() => setPlanet(2)} >EUROPA</Button>
-                  <Button variant="btn-outline" className="nav__link nav-link" onClick={() => setPlanet(3)} >TITAN</Button>
-                </Nav>
-            <h2 className="planet__header">{planets[planet].name}</h2>
-            <p className="planet__text">{planets[planet].text}</p>
-            </div> 
-            <hr className="my-4"/>
-            <div className="dest__info"> 
-           
-                <div className="distance__info">
-                    <p className="sub-head-1">AVG. DISTANCE</p>
-                    <p className="sub-head-2">{planets[planet].distance}</p>
+             <div className="planet__info-all">
+                    <div className="planet__info">
+                      <Nav className="nav__links">
+                        <Button variant="btn-outline" className="nav__link nav-link" onClick={() => setPlanet(0)} >MOON</Button>
+                        <Button variant="btn-outline" className="nav__link nav-link" onClick={() => setPlanet(1)} >MARS</Button>
+                        <Button variant="btn-outline" className="nav__link nav-link" onClick={() => setPlanet(2)} >EUROPA</Button>
+                        <Button variant="btn-outline" className="nav__link nav-link" onClick={() => setPlanet(3)} >TITAN</Button>
+                      </Nav>
+                      <h2 className="planet__header">{planets[planet].name}</h2>
+                      <p className="planet__text">{planets[planet].text}</p>
+                    </div> 
+                  <hr className="my-4"/>
+                <div className="dest__info">
+                    <div className="distance__info">
+                        <p className="sub-head-1">AVG. DISTANCE</p>
+                        <p className="sub-head-2">{planets[planet].distance}</p>
+                    </div>
+                    <div className="travel__info">
+                        <p className="sub-head-1">EST. TRAVEL TIME</p>
+                        <p className="sub-head-2">{planets[planet].travelTime}</p>
+                    </div>
                 </div>
-                <div className="travel__info">
-                    <p className="sub-head-1">EST. TRAVEL TIME</p>
-                    <p className="sub-head-2">{planets[planet].travelTime}</p>
-                </div></div>
                 
             </div>
             </Container>
